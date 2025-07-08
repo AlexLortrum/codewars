f8ee2db33cfa810df09ec2cbc6543a08c97f4360
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-08 12:01:48 +0300

    Create convert_boolean_values_to_strings_'Yes'_or_'No'.py

---
+++ 8kyu/Convert boolean values to strings 'Yes' or 'No'/convert_boolean_values_to_strings_'Yes'_or_'No'.py
@@ -0,0 +1,14 @@
+def bool_to_word(boolean):
+    bool_y_n = ['Yes','No']
+    ans = bool_y_n
+    if boolean == True:
+     	ans = ans[0]     
+    elif boolean == False:
+     	ans = ans[1]
+    return ans
+
+
+
+
+
+
